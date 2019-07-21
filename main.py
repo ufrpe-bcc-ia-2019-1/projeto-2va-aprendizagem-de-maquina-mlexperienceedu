1d24afc99bad17966b3de6ea8dbf7f246811a273
Tiago de Lima <tiago.blima@ufrpe.br>
2019-07-21 16:54:44 -0300

    "Arquivos em txt"

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from preprocess import *
 
-bible_1 = pd.read_csv('raw_data/tukano.csv')
+bible_1 = pd.read_csv('raw_data/guarani.csv')
 bible_2 = pd.read_csv('raw_data/portugues.csv')
 
 data = to_translation_format(bible_1['Scripture'], bible_2['Scripture'])
 
-to_file(data, path='raw_data/tu-pt')
+to_file(data, path='raw_data/gu-pt.txt')
