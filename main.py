eb1528c2e1787175463d0963e8a35f21af68d16a
Tiago de Lima <tiago.blima@ufrpe.br>
2019-07-26 08:17:37 -0300

    new files

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from preprocess import *
+import matplotlib.pyplot as plt
+import numpy as np 
 
 bible_1 = pd.read_csv('raw_data/guarani.csv')
 bible_2 = pd.read_csv('raw_data/portugues.csv')
@@ -7,4 +9,8 @@ data = to_translation_format(bible_1['Scripture'], bible_2['Scripture'])
 
 to_file(data, path='raw_data/gu-pt.txt')
 
-results = obtain_results('results.txt', ['time'])
+results = obtain_results('test_result.txt', ['acc'])
+
+plt.bar(np.arange(3), height=results['acc'])
+plt.xticks(np.arange(3),('Karajá', 'Tukano', 'Guarani'))
+plt.show()
