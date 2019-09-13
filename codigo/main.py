a8f140d8d40c9356df350ced48119990a542a2e2
Tiago de Lima <tiago.blima02@gmail.com>
2019-09-13 08:26:18 -0300

    Adicionando shuffle aos dados e corrigindo alguns erros

--- codigo/main.py
+++ codigo/main.py
@@ -1,22 +1,26 @@
 from util import prepData
 import matplotlib.pyplot as plt
-import numpy as np 
+import numpy as np
 import pandas as pd
 import re
 
-#--- Tirando referências -----
+# --- Tirando referências -----
 livros = []
 penta = ['Gênesis', 'Êxodo', 'Levítico', 'Números', 'Deuteronômio']
 
-history = ['Josué', 'Juízes', 'Rute', '1 e 2 Samuel', '1 e 2 Reis', '1 e 2 Crônicas', 'Esdras', 'Neemias', 'Tobias', 'Judite', 'Ester', '1 e 2 Macabeus']
+history = ['Josué', 'Juízes', 'Rute', '1 e 2 Samuel', '1 e 2 Reis', '1 e 2 Crônicas', 'Esdras', 'Neemias', 'Tobias',
+           'Judite', 'Ester', '1 e 2 Macabeus']
 
 poete = ['Jó', 'Salmo', 'Provérbios', 'Eclesiastes', 'Cântico dos Cânticos', 'Sabedoria', 'Eclesiástico']
 
-profe = ['Isaías', 'Jeremias', 'Lamentações', 'Baruc', 'Ezequiel', 'Daniel', 'Oséias', 'Joel', 'Amós', 'Abdias', 'Jonas', 'Miquéias', 'Naum', 'Habacuque', 'Sofonias', 'Ageu', 'Zacarias', 'Malaquias']
+profe = ['Isaías', 'Jeremias', 'Lamentações', 'Baruc', 'Ezequiel', 'Daniel', 'Oséias', 'Joel', 'Amós', 'Abdias',
+         'Jonas', 'Miquéias', 'Naum', 'Habacuque', 'Sofonias', 'Ageu', 'Zacarias', 'Malaquias']
 
 evan = ['Mateus', 'Marcos', 'Lucas', 'João']
 
-cartas = ['Atos', 'Romanos', '1 e 2 Coríntios', 'Gálatas', 'Efésios', 'Filipenses', 'Colossenses','1 e 2 Tessalonicenses', '1 e 2 Timóteo', 'Tito', 'Filemon', 'Hebreus', 'Tiago', '1 e 2 Pedro', '1 a 3 João', 'Judas', 'Apocalipse']
+cartas = ['Atos', 'Romanos', '1 e 2 Coríntios', 'Gálatas', 'Efésios', 'Filipenses', 'Colossenses',
+          '1 e 2 Tessalonicenses', '1 e 2 Timóteo', 'Tito', 'Filemon', 'Hebreus', 'Tiago', '1 e 2 Pedro', '1 a 3 João',
+          'Judas', 'Apocalipse']
 
 livros.extend(penta)
 livros.extend(history)
@@ -27,10 +31,10 @@ livros.extend(cartas)
 
 noise = []
 for livro in livros:
-    livro += ' [0-9]*.[0-9]*'
-    livro = livro.replace('1 e 2', '[0-9]')
-    livro = livro.replace('1 a 3', '[0-9]')
-    noise.append(livro)
+	livro += ' [0-9]*.[0-9]*'
+	livro = livro.replace('1 e 2', '[0-9]')
+	livro = livro.replace('1 a 3', '[0-9]')
+	noise.append(livro)
 noise.append('<.*?>|<.*?>.*?<.*?>')
 prep = prepData(['guarani', 'karaja', 'xavante', 'tukano', 'portugues'])
 
@@ -41,11 +45,11 @@ prep.set_sufix('.csv')
 datasets = prep.clean_data(noise)
 prep.save_all_datasets()
 
-gu_pt = prep.to_pair_format(datasets['guarani'], datasets['portugues'], 'Scripture')
-ka_pt = prep.to_pair_format(datasets['karaja'], datasets['portugues'], 'Scripture')
-xa_pt = prep.to_pair_format(datasets['xavante'], datasets['portugues'], 'Scripture')
-tu_pt = prep.to_pair_format(datasets['tukano'], datasets['portugues'], 'Scripture')
-
+gu_pt = prep.to_pair_format(datasets['guarani'], datasets['portugues'])
+ka_pt = prep.to_pair_format(datasets['karaja'], datasets['portugues'])
+xa_pt = prep.to_pair_format(datasets['xavante'], datasets['portugues'])
+tu_pt = prep.to_pair_format(datasets['tukano'], datasets['portugues'])
+print()
 names = ['gu-pt', 'ka-pt', 'xa-pt', 'tu-pt']
 pairs = [gu_pt, ka_pt, xa_pt, tu_pt]
 prep.set_prefix('../datasets/pairs/')
--- codigo/util.py
+++ codigo/util.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import re
+import random
+
 
 class prepData():
-      
   datasets = None
   datasets_names = []
   prefix = ''
@@ -15,7 +15,7 @@ class prepData():
 
   def set_prefix(self, path_prefix):
     self.prefix = path_prefix
-  
+
   def set_dataset_names(self, datasets_names):
     self.datasets_names = datasets_names
 
@@ -24,19 +24,19 @@ class prepData():
 
   def get_prefix(self, path_prefix):
     return self.prefix
-  
+
   def get_dataset_names(self, datasets_names):
-    return self.datasets_names 
+    return self.datasets_names
 
   def get_sufix(self, file_sufix):
-    return self.sufix 
-  
+    return self.sufix
+
   def read_all(self, regex=[]):
-              
+
     for name in self.datasets_names:
-          
+
       path = self.prefix + name + self.sufix
-      
+
       try:
         dataset = pd.read_csv(path, encoding='utf-8')
         self.datasets[name] = dataset
@@ -46,11 +46,11 @@ class prepData():
     return self.datasets
 
   def clean_data(self, regex=[]):
-            
+
     for name in self.datasets_names:
-          
+
       path = self.prefix + name + self.sufix
-      
+
       try:
         dataset = pd.read_csv(path, encoding='utf-8')
       except:
@@ -58,14 +58,14 @@ class prepData():
 
       for exp in regex:
         dataset = dataset.replace(to_replace=exp, value='', regex=True)
-        
+
       self.datasets[name] = dataset
-      path = path.replace(name + self.sufix,'')
+      path = path.replace(name + self.sufix, '')
 
     return self.datasets
 
-  def save_pairs(self,file_names=[], texts=[]):
-    
+  def save_pairs(self, file_names=[], texts=[]):
+
     for file_name, text in zip(file_names, texts):
       path = self.prefix + file_name + self.sufix
 
@@ -73,60 +73,58 @@ class prepData():
 
       for line in text:
         file.write(line)
-      
+
       file.close()
 
   def save_all_datasets(self):
-        
+
     for name, data in zip(self.datasets_names, self.datasets.values()):
       path = self.prefix + name + self.sufix
-      
-      data.to_csv(path, index=False)
-      path = path.replace(name + self.sufix,'')
 
-  def to_pair_format(self, raw_1, raw_2, data_index):
+      data.to_csv(path, index=False)
+      path = path.replace(name + self.sufix, '')
+
+  def to_pair_format(self, raw_1, raw_2, data_index='Scripture'):
 
     pair_text = []
 
     for r_1, r_2 in zip(raw_1[data_index], raw_2[data_index]):
-  
       r_1 = ' '.join(str(r_1).split())
       r_2 = ' '.join(str(r_2).split())
-
       pair_text.append(r_1 + '\t' + r_2 + '\n')
 
+    random.shuffle(pair_text)
     return pair_text
 
 
+class Result:
 
-class result():
-      
-  def calculate_time(self, results = {}):
-        
+  def calculate_time(self, results={}):
+
     time_taken = 0
 
     for t in results['time']:
       time_taken += t
 
-    print("Total time taken: {} sec {} hs".format(time_taken,time_taken/3600))
+    print("Total time taken: {} sec {} hs".format(time_taken, time_taken / 3600))
 
-  def obtain_results(self, file_name, param = []):
+  def obtain_results(self, file_name, param=[]):
 
     results = {}
 
     for p in param:
-        results[p] = []
+      results[p] = []
 
     file = open(file_name, 'r')
-  
+
     line = file.readline()
     while line:
-      
+
       data = line.lower().split()
 
       for p in param:
         index = data.index(p)
-        results[p].append(float(data[index+1]))
+        results[p].append(float(data[index + 1]))
 
       line = file.readline()
 
@@ -141,19 +139,17 @@ class result():
     plt.ylabel(ylabel)
     plt.title(title)
     plt.show()
-  
+
   def save_epoch_results(self, epoch, loss, time):
-    
+
     file = open('results.txt', 'a')
-    file.write('epoch ' + epoch + ' loss ' + loss + ' time ' + time )
+    file.write('epoch ' + epoch + ' loss ' + loss + ' time ' + time)
     file.close()
 
   def saving_result(self, treino, teste, acc):
     path = "raw_data/results/"
-    file = open(path+"test_result.txt", 'a')
+    file = open(path + "test_result.txt", 'a')
 
     file.write('Train ' + str(treino) + '\tTest ' + str(teste) + '\tAcc ' + str(acc) + '\n')
 
     file.close()
-    
-
