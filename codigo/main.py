493af6a636545745e36940817d7b3ecc268f0ebb
Tiago de Lima <tiago.blima@ufrpe.br>
2019-10-21 21:34:59 -0300

    - Updating main.py

--- codigo/main.py
+++ codigo/main.py
@@ -1,3 +1,8 @@
 from util.preprocess import PrepData
+import pandas as pd
 
+import os
 
+print(os.listdir('datasets/'))
+bible = pd.read_csv('datasets/portugues.csv')
+bible_text = bible['Scripture']
